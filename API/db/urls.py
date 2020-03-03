bb6c1712d8be1e289a09b2c57a88ee2548d8a02b
zicanl <zicanl@uci.edu>
2020-03-02 19:58:58 -0800

    add ChangePasswordAPI and SignInAPI, add correctness check at CreateUserAPI

--- API/db/urls.py
+++ API/db/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
 
     path('character', GetCharacter.as_view(), name="getCharacter"),
     path('monster', GetMonster.as_view(), name="getMonster"),
-    path('item', GetMonster.as_view(), name="getMonster")
+    path('item', GetMonster.as_view(), name="getMonster"),
+    path('change_password', ChangePasswordAPI.as_view(), name="changePassword"),
+    path('sign_in', SignInAPI.as_view(), name="signIn")
 ]
--- API/db/views.py
+++ API/db/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.generics import (CreateAPIView, GenericAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView)
 from db.models import *
+from django.db import IntegrityError
 
 
 def index(request):
@@ -38,20 +39,68 @@ class CreateUserAPI(GenericAPIView):
     #     if not self.is_manager():
     #         courses = courses.filter(university_school=self.request.user)
     #     return courses
-    #
-    # def get(self, request, school_id, *args, **kwargs):
-    #     self.school_id = school_id
-    #     return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        # try:
 
-        user = UserProfile(username=request.data['name']
-                           , password=request.data['password']
+        username = request.data['name']
+        password = request.data['password']
+
+        # raise Exception when username is too short
+        if len(username) < 4:
+            return HttpResponse(Exception("user name too short"), status=400)
+        # raise Exception when password is too short
+        if len(password) < 4:
+            return HttpResponse(Exception("password too short"), status=400)
+
+        user = UserProfile(username=username
+                           , password=password
                            , currency=0
                            )
-        user.save()
+        try:
+            user.save()
+        except IntegrityError as e:
+            return HttpResponse("Duplicated username", status=400)
         # except Exception as e:
         #     return HttpResponse(e, status=500)
 
         return HttpResponse("User Created", status=200)
+
+
+class ChangePasswordAPI(GenericAPIView):
+
+    def post(self, request, *args, **kwargs):
+        username = request.data['name']
+        oldPassword = request.data['oldPassword']
+        newPassword = request.data['newPassword']
+        # raise Exception when password is too short
+        if len(newPassword) < 4:
+            return HttpResponse(Exception("password too short"), status=400)
+
+        # check if user exists or not
+        try:
+            user = UserProfile.objects.get(username=username)
+        except UserProfile.DoesNotExist:
+            return HttpResponse("User does not exists", status=400)
+
+        # check the correctness of the old password
+        if user.password != oldPassword:
+            return HttpResponse("Password is wrong", status=400)
+
+        user.password = newPassword
+        user.save()
+        return HttpResponse("Password changed", status=200)
+
+
+class SignInAPI(GenericAPIView):
+
+    def post(self, request, *args, **kwargs):
+        username = request.data['name']
+        password = request.data['password']
+
+        # check if the username and password
+        try:
+            UserProfile.objects.get(username=username, password=password)
+        except UserProfile.DoesNotExist:
+            return HttpResponse("Wrong username or password", status=400)
+
+        return HttpResponse("Signed in", status=200)
