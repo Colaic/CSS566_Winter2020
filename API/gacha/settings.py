45ad722aa1f64de9a174efb6ed5bfe050df34043
goujiashun@gmail.com <goujiashun@gmail.com>
2020-03-02 21:25:43 -0800

    v0.2.1 add example API test add django_extensions amend requirements.txt

--- API/gacha/settings.py
+++ API/gacha/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['0.0.0.0',
 INSTALLED_APPS = [
     'db',
     'rest_framework',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
