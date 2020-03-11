9101119797414424dc1f9290d515f4b425243ba1
Ardalan Ahanchi <ardalan@ahanchi.net>
2020-03-11 04:08:34 -0700

    Re-wrote the API (front-end and back-end no-databse) stack for login and signup. Added the new database-less API to the project, tested and working

--- FrontEnd/Api.gd
+++ FrontEnd/Api.gd
@@ -4,7 +4,8 @@ extends Node
 var _http = null
 var _user = null
 var _pass = null
-const _URL = "18.191.61.189"
+#const _URL = "18.191.61.189"
+const _URL = "0.0.0.0"
 const _PORT = 8888
 
 # Called when the node enters the scene tree for the first time.
@@ -19,9 +20,7 @@ func _init():
 		_http.poll()
 		OS.delay_msec(500)
 		
-	#Check if we're connected.
-	assert(_http.get_status() == HTTPClient.STATUS_CONNECTED)
-
+#A method which authenticates the user.
 func login(username, password):
 	#Set the username and password.
 	_user = username
@@ -30,20 +29,18 @@ func login(username, password):
 	#Print out the login message for logging purposes.
 	print("Logging in user " + _user + "...")
 	
-	#Create the request with the given parameters, and make the request.
-	var fields = {"name" : _user, "password" : _pass}
-	var query = _http.query_string_from_dict(fields)
-	var headers = ["Content-Type: application/x-www-form-urlencoded", "user-agent: GotchaGame", "Content-Length: " + str(query.length())]
-	var results = _http.request(_http.METHOD_POST, "/db/user_auth", headers, query)
+	#Create the request with the given parameters, and make the post request.
+	var data = {"name" : _user, "password" : _pass}
+	var post_response = _post(data, "/db/user_auth")
 	
-	#Check if the results are OK
-	#return results == OK
+	#Parse the JSON message, and check the response code.
+	var msg = _get_response_msg(post_response)
+	if(post_response["code"] != 200):
+		return {"success" : false , "message" : msg}
 	
-	#TODO: Use the above line instead of alwasy returning false after sever
-	#Supports this end-point. Currently it does not work.
-	return false
-	
-	
+	#If we get here, it was successful.
+	return {"success" : true, "message" : msg}
+
 # A method for signing up the user
 func signup(username, password):
 	#Set the username and password.
@@ -53,27 +50,65 @@ func signup(username, password):
 	#Print out the signup message for logging purposes.
 	print("Signing up user " + _user + "...")
 	
-	#Create the request with the given parameters, and make the request.
-	var fields = {"name" : _user, "password" : _pass}
-	var query = _http.query_string_from_dict(fields)
-	var headers = ["Content-Type: application/x-www-form-urlencoded", "user-agent: GotchaGame", "Content-Length: " + str(query.length())]
-	var results = _http.request(_http.METHOD_POST, "/db/user_create", headers, query)
+	#Create the request with the given parameters, and make the post request.
+	var data = {"name" : _user, "password" : _pass}
+	var post_response = _post(data, "/db/user_create")
 	
-	#Check if the results are OK
-	return results == OK
+	#Parse the JSON message, and check the response code.
+	var msg = _get_response_msg(post_response)
+	if(post_response["code"] != 200):
+		return {"success" : false , "message" : msg}
 	
-	## Some code for reading back data from the HTTP connection.
-	#
-	#var rb = PoolByteArray() # Array that will hold the data.
-	#
-	#while _http.get_status() == HTTPClient.STATUS_BODY:
-	#	# While there is body left to be read
-	#	_http.poll()
-	#	var chunk = _http.read_response_body_chunk() # Get a chunk.
-	#	if chunk.size() == 0:
-	#		# Got nothing, wait for buffers to fill a bit.
-	#		OS.delay_usec(1000)
-	#	else:
-	#		rb = rb + chunk # Append to read buffer.
-	#
-	#print(rb.get_string_from_utf8())
+	#If we get here, it was successful.
+	return {"success" : true, "message" : msg}
+
+#A function which returns the message with key "status" from the server.
+func _get_response_msg(post_response):
+	return JSON.parse(post_response["json"]).result["status"]
+
+# A method which makes post requests to the server, and sends JSON over.
+func _post(data, url):
+	#If we got disconnected, reconnect.
+	if(_http.get_status() != HTTPClient.STATUS_CONNECTED):
+		_init()
+		
+	#Create a query, and then create a post request to the server.
+	var query = JSON.print(data)
+	var headers = ["Content-Type: application/json", "user-agent: GotchaGame"]
+	var results = _http.request(_http.METHOD_POST, url, headers, query)
+	
+	#Check the return value.
+	if(results != OK):
+		return {"code" : 400, "json" : JSON.print({"status" : "Request Failed."})}
+
+	#Wait till the request is completed.
+	while (_http.get_status() == HTTPClient.STATUS_REQUESTING):
+		_http.poll()
+		OS.delay_msec(500)
+	
+	#If we get disconnected return an error.
+	if(_http.get_status() != HTTPClient.STATUS_BODY and 
+	_http.get_status() != HTTPClient.STATUS_CONNECTED):
+		return {"code" : 400, "json" : JSON.print({"status" : "Connection Failed."})}
+	
+	#If we didn't get a response, return error.
+	if (!_http.has_response()):
+		return {"code" : 400, "json" : JSON.print({"status" : "No Response From Server."})}
+	
+	#If we didn't get a 200 status, return the error.
+	#if (_http.get_response_code() != 200):
+	#	return {"code" : _http.get_response_code(), "json" : JSON.print({"status" : "Request Failed."})}
+		
+	#If everything went fine, poll and recieve all the data from server.
+	var res_data = PoolByteArray()
+	while (_http.get_status() == HTTPClient.STATUS_BODY):
+		_http.poll()
+		var chunk = _http.read_response_body_chunk()
+		if chunk.size() == 0:
+			OS.delay_usec(500)
+		else:
+			res_data = res_data + chunk
+	
+	#Convert the data to text, and return it with the response code.
+	var res_data_text = res_data.get_string_from_utf8()
+	return {"code" : _http.get_response_code(), "json" : res_data_text}
--- FrontEnd/WelcomeScene.gd
+++ FrontEnd/WelcomeScene.gd
@@ -25,26 +25,26 @@ func _on_SignupButton_button_up():
 	#Get the object for the API.
 	var server = get_node("/root/Api")
 	
-	#Call signup from the API, and check if it was successufl.
-	if(server.signup(get_username(), get_password())):
-		#Change the scene if successful.
-		get_tree().change_scene("res://UserProfile.tscn")
-	else:
-		set_status("Signup Failed")
+	#Call signup from the API, and get it's response.
+	var response = server.signup(get_username(), get_password())
+	
+	#Print the response message, and wait for user to login.
+	set_status(response["message"])
 	
 	
 #A function which runs when the login button is pressed.
 func _on_LoginButton_button_up():	
-	
 	#Get the object for the API.
 	var server = get_node("/root/Api")
-	
-	#Call signup from the API, and check if it was successufl.
-	if(server.login(get_username(), get_password())):
-		#Change the scene if successful.
-		get_tree().change_scene("res://UserProfile.tscn")
+
+	#Call login from the API, and get it's response.	
+	var response = server.login(get_username(), get_password())
+
+	#Change the scene if successful.
+	if(response["successful"]):
+		get_tree().change_scene("res://Homepage.tscn")
 	else:
-		set_status("Login Failed")
+		set_status(response["message"])
 	
 	
 #A function which runs when the exit button is pressed.
