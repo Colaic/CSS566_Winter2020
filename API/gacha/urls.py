b35bbf08fb1869cdf8c213247708109c9491f648
enjoybeta <goujiashun@gmail.com>
2020-03-12 18:01:36 -0700

    API v0.3.2
    remove '/' to make endpoints consistent

--- API/gacha/urls.py
+++ API/gacha/urls.py
@@ -26,7 +26,7 @@ admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('db/', include('db.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
 ]
