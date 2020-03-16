a871b1aeabe31a62d571d53391067a26b1df4f84
Anish Prasad <33945547+anishprasad01@users.noreply.github.com>
2020-03-16 14:45:32 -0700

    Modified store script to automatically give 1000 coins on entry

--- FrontEnd/StorePurchase.gd
+++ FrontEnd/StorePurchase.gd
@@ -11,6 +11,7 @@ var rng = RandomNumberGenerator.new()
 # enter store scene with updated userCurrency
 func _ready():
 	#randomize() # somehow this is doesn't work as the seed is always the same...
+	server.set_currency(1000);
 	var server = get_node("/root/Api")
 	userCurrency = server.get_currency()
 	get_node("CurrencyWidget/RichTextLabel").text = str(userCurrency)
