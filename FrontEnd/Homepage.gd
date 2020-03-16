9ab3bf825e2b7ddc7ef4bbe57972781c30b08e45
Anish Prasad <33945547+anishprasad01@users.noreply.github.com>
2020-03-16 15:52:46 -0700

    Updates, See Description
    Removed HP and MP bars on homepage
    Added username and current level to homepage
    Added script to get username and current level from db via api
    Re-enabled randomaization for store summoning

--- FrontEnd/Homepage.gd
+++ FrontEnd/Homepage.gd
@@ -4,14 +4,16 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var currency
-
+onready var server = get_node("/root/Api")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var UserLabel = get_node("HomePage/UsernameField")
+	UserLabel.text = str(server._user)
 	
+	var MissionLabel = get_node("HomePage/MissionField")
+	MissionLabel.text = str(server.get_mission_num())
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
